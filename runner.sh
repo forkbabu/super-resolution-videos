80cac564cce5b31ea7a52627172cf895f2dd6271
Forkbabu <sayantandas30011998@gmail.com>
2020-06-01 14:13:54 +0530

    Create runner.sh

---
+++ runner.sh
@@ -0,0 +1,4 @@
+/opt/conda/bin/python3.7 -m pip install --upgrade pip
+pip3 install -r kaggle.txt
+wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
